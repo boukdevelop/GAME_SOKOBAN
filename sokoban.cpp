ddab5438bf9a4103c3582df11b73b1697ea39427
FRANCK <boukalafranck0@gmail.com>
2026-03-05 16:04:18 +0100

    Ajout d'une image à la place du rectangle coloré

--- sokoban.cpp
+++ sokoban.cpp
@@ -11,25 +11,34 @@ int main(int argc, char *argv[])
 
     if (!SDL_Init(SDL_INIT_VIDEO))
     {
-        std::cout << "Erreur lors de l'initialisation de la fenêtre : " << SDL_GetError();
+        std::cout << "Erreur lors de l'initialisation de la SDL : " << SDL_GetError();
         return 1;
     }
 
     int winW = 800, winH = 600;
+
+    // CHargement de la fenetre
     SDL_Window *window = SDL_CreateWindow(
         "SOKOBAN - GAME",
         winW,
         winH,
         SDL_WINDOW_RESIZABLE);
 
+    // Chargement du rendu
     SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
 
+    // CHargement de la texture
+    SDL_Texture *playtexture = IMG_LoadTexture(renderer, "assests/logo.png");
+
+    if (!playtexture)
+        SDL_Log("Impossible de charger l'image : %s", SDL_GetError());
+    // Si l'image ne charge pas, on affichera quand même un rectangle
     SDL_Event event;
     bool run = true;
 
-    // Creation du personnage
+    // Creation du personnage (Rectangle de destination)
     SDL_FRect play{300.0f, 200.0f, 70.0f, 70.0f};
-    float speed = 5.0f;       // Vitesse de deplacement
+    float speed = 40.0f;      // Vitesse de deplacement
     bool Isfollowing = false; // Vérifit si le rect suit la souris
 
     while (run)
@@ -86,25 +95,56 @@ int main(int argc, char *argv[])
             }
 
             // Gestion du rectangle par le motion du curseur (Glisser le rectangle)
-            if (event.type == SDL_EVENT_KEY_DOWN)
+            if (event.type == SDL_EVENT_MOUSE_BUTTON_DOWN)
             {
+                if (event.button.button == SDL_BUTTON_RIGHT)
+                    Isfollowing = !Isfollowing; // Alternes entre true et false
+                if (event.button.button == SDL_BUTTON_LEFT)
+                    Isfollowing = true; // Boutton gauche active le suivie
+            }
+
+            if (event.type == SDL_EVENT_MOUSE_BUTTON_UP)
+            {
+                if (event.button.button == SDL_BUTTON_LEFT)
+                    Isfollowing = false; // Boutton droit desactive le suivi
+            }
+
+            // Deplacer le rectangle si la touche gauche est active if (event.type == SDL_EVENT_MOUSE_MOTION && Isfollowing)
+            if (event.type == SDL_EVENT_MOUSE_MOTION && Isfollowing)
+            {
+                // laisser le curseur au milieu du rect
+                play.x = event.motion.x - (play.w / 2);
+                play.y = event.motion.y - (play.h / 2);
             }
         }
-        
+
         // Rendu Graphique
         SDL_SetRenderDrawColor(renderer, 100, 200, 90, 255);
         SDL_RenderClear(renderer);
 
-        // Rendu du rectangle
-        SDL_SetRenderDrawColor(renderer, 200, 100, 99, 255);
-        SDL_RenderFillRect(renderer, &play);
+        if (playtexture)
+        {
+            // SDL3 : Dessin de la texture dans le rectangle
+            // NULL : On dessine toute l'image source
+            SDL_RenderTexture(renderer, playtexture, NULL, &play);
+        }
+        else
+        {
+            SDL_SetRenderDrawColor(renderer, 255, 129, 200, 255);
+            SDL_RenderFillRect(renderer, &play);
+        }
+
+        // // Rendu du rectangle
+        // SDL_SetRenderDrawColor(renderer, 200, 100, 99, 255);
+        // SDL_RenderFillRect(renderer, &play);
 
         // Présentation du graphique
         SDL_RenderPresent(renderer);
     }
 
-    SDL_DestroyWindow(window);
+    SDL_DestroyTexture(playtexture);
     SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
     SDL_Quit();
 
     return 0;
